5ab71dd2b7439774ab3a482fd828173968bfbc2f
Moussa <konatemoussa8348@gmail.com>
2026-08-12 11:27:51 +0000

    Préparation finale pour Render : settings, requirements, build.sh et export des données

--- boutique_alimentation/boutique_alimentation/settings.py
+++ boutique_alimentation/boutique_alimentation/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os  # Si elle n'est pas déjà présente
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,8 +37,11 @@ SECRET_KEY = 'django-insecure-p_)jd76dxd)f3sg77900#0i(4d!_vi8_m=u(x8w#z65x752'\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['*']  # On sécurisera plus tard avec le vrai nom
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # Application definition
 
@@ -49,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'boutique_alimentation.urls'
@@ -76,10 +91,11 @@ WSGI_APPLICATION = 'boutique_alimentation.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -123,6 +139,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -137,7 +156,7 @@ LOGIN_URL = 'connexion'
 
 # Dossier pour les fichiers uploadés (images, avatars)
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # URL de redirection pour les vues @login_required
 LOGIN_URL = 'connexion'
