7722ad1bb7b188fb9aeadfb0d3bbfdcc415f0dce
Moussa <konatemoussa8348@gmail.com>
2026-08-17 10:06:27 +0000

    Ajout temporaire de création auto du superutilisateur

--- boutique_alimentation/boutique_alimentation/settings.py
+++ boutique_alimentation/boutique_alimentation/settings.py
@@ -113,3 +113,22 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_URL = "connexion"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+# =========================================================
+# CRÉATION AUTO D'UN SUPERUTILISATEUR (à supprimer après)
+# =========================================================
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@example.com',
+        password='admin123!'  # Changez ce mot de passe
+    )
+    print("✅ Superutilisateur 'admin' créé avec succès !")
+else:
+    print("ℹ️ Le superutilisateur existe déjà.")
